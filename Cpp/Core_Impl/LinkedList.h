a41d88e6fd7db67236611d4d2b6a12be6b8768ab
ashishmodi <gashishmukesh92@gmail.com>
2017-03-19 13:17:56 +0530

    Added reverse delete API's

--- Cpp/Core_Impl/LinkedList.h
+++ Cpp/Core_Impl/LinkedList.h
@@ -24,7 +24,19 @@ public:
 	void insertAtNth(int no, int pos);
 	void insertAtEnd(int no);
 
-	//Helper APIs
+	//Delete APIs
+	void deleteFromNth(int pos);
+
+	//Reverse APIs
+	void reverseLL_Iterative();
+	void reverseLL_Recursion(Node* p);
+	
+	//Print APIs
 	void printLinkedList();
+	void printLL_recursion(Node* head); //Initially we pass address of head node
+	void printLL_Reverse_recursion(Node* head);
+
+	//Helper APIs	
 	int noOfElementsInLL();
 };
+

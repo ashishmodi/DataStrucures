abf901fc5f0fff8d123c0e4a88d4f9c30fcb218b
ashishmodi <gashishmukesh92@gmail.com>
2018-12-06 23:37:05 +0530

    Delete node, Reverse doubly LL

--- Cpp/Core_Impl/DoublyLL.cpp
+++ Cpp/Core_Impl/DoublyLL.cpp
@@ -13,6 +13,20 @@ DoublyLL::~DoublyLL()
 
 void DoublyLL::testDoublyLL()
 {
+	insertAtBegining(5);
+	insertAtBegining(10);
+	insertAtEnd(15);
+	insertAtNth(20, 2);
+	insertAtNth(25, 3);
+	insertAtNth(30, 6);
+	insertAtNth(35, 4);
+	printDoublyLL();
+	deleteFromNth(1);
+	deleteFromNth(4);
+	deleteFromNth(2);
+	printDoublyLL();
+	ReverseDoublyLL_Iterative();
+	printDoublyLL();
 }
 
 Node* DoublyLL::createNode(int data)
@@ -36,3 +50,92 @@ void DoublyLL::insertAtBegining(int no)
 	temp->next = head;
 	head = temp;
 }
+
+void DoublyLL::insertAtNth(int no, int pos)
+{
+	Node* newNode = createNode(no);
+	Node* temp = head;
+
+	if (temp == NULL || pos == 1)
+	{
+		insertAtBegining(no);
+		return;
+	}
+	
+	for (int i = 0; i < pos-2; i++)
+	{
+		temp = temp->next;
+	}
+	if (temp->next == NULL)
+	{
+		insertAtEnd(no);
+		return;
+	}
+	newNode->next = temp->next;
+	temp->next->prev = newNode;
+	temp->next = newNode;
+	newNode->prev = temp;
+}
+
+void DoublyLL::insertAtEnd(int no)
+{
+	Node* temp = head;
+	while (temp->next != NULL)
+		temp = temp->next;
+	Node* newNode = createNode(no);
+	temp->next = newNode;
+	newNode->prev = temp;
+}
+
+void DoublyLL::deleteFromNth(int pos)
+{
+	Node* temp = head;
+	if (pos == 1)
+	{
+		head = temp->next;
+		head->prev = NULL;
+		delete temp;
+		return;
+	}
+	for (int i = 0; i < pos - 2 ; i++)
+	{
+		temp = temp->next;
+	}
+	Node* temp2 = temp->next;
+	temp->next = temp2->next;
+	temp2->next->prev = temp;
+	delete temp2;
+}
+
+void DoublyLL::ReverseDoublyLL_Iterative()
+{
+	Node* curr = head;
+	if (curr == NULL || curr->next == NULL)
+		return;		// No Node or only 1 Node
+	while (curr->next != NULL)
+		curr = curr->next;
+	
+	head = curr;	
+	Node* temp;
+
+	while (curr != NULL)
+	{
+		temp = curr->next;
+		curr->next = curr->prev;
+		curr->prev = temp;
+		curr = curr->next;
+	}
+	head->prev = NULL;
+}
+
+void DoublyLL::printDoublyLL()
+{
+	Node* temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
